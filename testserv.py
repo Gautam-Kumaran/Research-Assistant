9351cd9e7514459e51af9f2087c15eddae4bfbd1
Gautam <gautamkumaran00@gmail.com>
2023-08-02 08:32:20 +0530

    cleaning up code

--- testserv.py
+++ testserv.py
@@ -1,16 +1,8 @@
 import requests
 
-print(" *********** going  got this ")
-
-print(
-    requests.get(        "http://127.0.0.1:8000"    ).content
-)
-
-print(" sfsdf  got this ")
-
 print(
     requests.post(
-        "http://127.0.0.1:8000",
+        "https://research-assistant-vh2e.onrender.com",
         json={
             "query": "What is meta's new product Thread?"
         }
